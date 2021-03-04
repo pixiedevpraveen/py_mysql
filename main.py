345511da1615fb5cc9c6baab1167e9ced60dc224
pixiedevpraveen <praveenyadav7787@gmail.com>
2021-03-05 00:25:07 +0530

    made a table called book(booid, title and price)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import mysql.connector
+
 from pr import pwd
 
-myDb = mysql.connector.connect(host='localhost', user='root', password=pwd)
-myCursor = myDb.cursor()
+myDb = mysql.connector.connect(
+    host='localhost', user='root', password=pwd, database='my_db1')
 
-myCursor.execute("CREATE DATABASE my_db1")
+myCursor = myDb.cursor()
+command = "CREATE TABLE book(bookid integer(4), title varchar(20), price float(5,2))"
+myCursor.execute(command)
