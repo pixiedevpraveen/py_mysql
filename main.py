136556cfc8f58f8c33249adf7563a0be90d52396
pixiedevpraveen <praveenyadav7787@gmail.com>
2021-03-05 01:18:47 +0530

    add values to book table in my_db1 database and commit it for reflect changes in database

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ myDb = mysql.connector.connect(
     host='localhost', user='root', password=pwd, database='my_db1')
 
 myCursor = myDb.cursor()
-command = "CREATE TABLE book(bookid integer(4), title varchar(20), price float(5,2))"
-myCursor.execute(command)
+command = "INSERT INTO book (bookid, title, price) VALUES(%s, %s, %s)"
+
+sno = 1
+book_name = "Let us C"
+price = 275
+
+# tuple
+book1 = (sno, book_name, price)
+
+# for execute the query
+myCursor.execute(command, book1)
+
+# for storing data or reflect changes to created database
+myDb.commit()
